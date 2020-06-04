c55a7cd4e4ead330083b14474fe557a99f540191
Chad H <chadg1980@gmail.com>
2020-06-04 19:15:28 -0400

    I got count sort down.

--- topscore/topscore.py
+++ topscore/topscore.py
@@ -2,7 +2,30 @@ import unittest
 
 
 def sort_scores(unsorted_scores, highest_possible_score):
-   
+
+    # if the length is 0 or 1, we can return. There is nothing to sort.
+    if len(unsorted_scores) <= 1:
+        return unsorted_scores
+
+    # Create a list to hold all possible scores in O(m) space where m is the size of highest_possible_score
+    all_scores = [0] * highest_possible_score
+
+    # For each score in unsorted scores, add it to the all_scores list. this is O(n) time
+    # where n is the size of unsorted_scores
+    for score in unsorted_scores:
+        all_scores[score] += 1
+
+    # sortedscores is a list to hold the scores in sorted order
+    sortedscores = []
+
+    # Loop through all the scores in the high score list is O(m) time
+    # If there is a score at that number, we add it to the sortedscores list
+    for score in range(len(all_scores) - 1, -1, -1):
+        if(all_scores[score] > 0):
+            for i in range(all_scores[score]):
+                sortedscores.append(score)
+
+    return sortedscores
 
 # Tests
 
