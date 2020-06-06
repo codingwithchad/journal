561077a75ea0eb006047232057d2c6e987249529
Chad H <chadg1980@gmail.com>
2020-06-06 13:09:11 -0400

    Used Greedy Algorithm.

--- stock_price/stockprice.py
+++ stock_price/stockprice.py
@@ -1,19 +1,17 @@
 def get_max_profit(stock_prices):
 
     if len(stock_prices) <= 1:
-        raise Exception
+        raise Exception("We need at least 2 times, 1 for purchase, one for selling")
     # Calculate the max profit
-    min_price = stock_prices[0]
-    max_price = stock_prices[0]
+    purchase_price = stock_prices[0]
+    max_profit = stock_prices[1] - stock_prices[0]
 
-    for price in stock_prices:
-        if price < min_price:
-            min_price = price
-        if price > max_price:
-            max_price = price
+    for i in range(1, len(stock_prices)):
+        max_profit = max(max_profit, (stock_prices[i] - purchase_price))
+        purchase_price = min(purchase_price, stock_prices[i])
 
 
-    return max_price - min_price
+    return max_profit
 
 
 # Tests
@@ -22,38 +20,38 @@ import unittest
 
 class Test(unittest.TestCase):
 
-    # def test_price_goes_up_then_down(self):
-    #     actual = get_max_profit([1, 5, 3, 2])
-    #     expected = 4
-    #     self.assertEqual(actual, expected)
-    #
-    # def test_price_goes_down_then_up(self):
-    #     actual = get_max_profit([7, 2, 8, 9])
-    #     expected = 7
-    #     self.assertEqual(actual, expected)
-    #
-    # def test_price_goes_up_all_day(self):
-    #     actual = get_max_profit([1, 6, 7, 9])
-    #     expected = 8
-    #     self.assertEqual(actual, expected)
-    #
+    def test_price_goes_up_then_down(self):
+        actual = get_max_profit([1, 5, 3, 2])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_down_then_up(self):
+        actual = get_max_profit([7, 2, 8, 9])
+        expected = 7
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_up_all_day(self):
+        actual = get_max_profit([1, 6, 7, 9])
+        expected = 8
+        self.assertEqual(actual, expected)
+
     def test_price_goes_down_all_day(self):
         actual = get_max_profit([9, 7, 4, 1])
         expected = -2
         self.assertEqual(actual, expected)
 
-    # def test_price_stays_the_same_all_day(self):
-    #     actual = get_max_profit([1, 1, 1, 1])
-    #     expected = 0
-    #     self.assertEqual(actual, expected)
+    def test_price_stays_the_same_all_day(self):
+        actual = get_max_profit([1, 1, 1, 1])
+        expected = 0
+        self.assertEqual(actual, expected)
 
-    # def test_error_with_empty_prices(self):
-    #     with self.assertRaises(Exception):
-    #         get_max_profit([])
+    def test_error_with_empty_prices(self):
+        with self.assertRaises(Exception):
+            get_max_profit([])
 
-    # def test_error_with_one_price(self):
-    #     with self.assertRaises(Exception):
-    #         get_max_profit([1])
+    def test_error_with_one_price(self):
+        with self.assertRaises(Exception):
+            get_max_profit([1])
 
 
 unittest.main(verbosity=2)
