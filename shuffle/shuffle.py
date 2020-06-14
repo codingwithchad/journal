a311e3f78080070fa4faf852a9aac077d72db864
Chad H <chadg1980@gmail.com>
2020-06-14 18:51:36 -0400

    grabbed stub code

---
+++ shuffle/shuffle.py
@@ -0,0 +1,21 @@
+import random
+
+
+def get_random(floor, ceiling):
+    return random.randrange(floor, ceiling + 1)
+
+
+def shuffle(the_list):
+
+    # Shuffle the input in place
+
+
+    pass
+
+
+sample_list = [1, 2, 3, 4, 5]
+print('Sample list:', sample_list)
+
+print('Shuffling sample list...')
+shuffle(sample_list)
+print(sample_list)
