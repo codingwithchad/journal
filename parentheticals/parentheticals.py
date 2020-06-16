71c47ee1ede16b34c1fde4caaa2ee3ad712d27d2
Chad H <chadg1980@gmail.com>
2020-06-16 18:53:56 -0400

    O(n) time and space

--- parentheticals/parentheticals.py
+++ parentheticals/parentheticals.py
@@ -1,17 +1,24 @@
 import unittest
 
-
+'''
+Write a function that, given a sentence , along with the position of an opening parenthesis, 
+finds the corresponding closing parenthesis.
+'''
 def get_closing_paren(sentence, opening_paren_index):
-
+    # ()()((()()))', 5
     # Find the position of the matching closing parenthesis
+    count = 0
+    for position in range(opening_paren_index + 1, len(sentence)):
+        char = sentence[position]
 
+        if char == '(':
+            count += 1
+        elif char == ')':
+            if count == 0:
+                return position
+            count -= 1
 
-    return -1
-
-
-
-
-
+    raise Exception
 
 
 
@@ -33,7 +40,6 @@ class Test(unittest.TestCase):
         actual = get_closing_paren('((((()))))', 2)
         expected = 7
         self.assertEqual(actual, expected)
-
 
     def test_mixed_openers_and_closers(self):
         actual = get_closing_paren('()()((()()))', 5)
