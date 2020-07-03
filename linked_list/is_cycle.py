72c7c0b0dbbd916aa480a2f61be7d79ba229efca
Chad H <chadg1980@gmail.com>
2020-07-03 09:05:31 -0400

    all ok tests

--- linked_list/is_cycle.py
+++ linked_list/is_cycle.py
@@ -12,10 +12,11 @@ def contains_cycle(first_node):
     if slow_node == fast_node:
         return True
     while fast_node.next:
-        if slow_node == fast_node:
-            return True
         fast_node = fast_node.next.next
         slow_node = slow_node.next
+
+        if slow_node == fast_node:
+            return True
 
 
     return False
@@ -47,51 +48,51 @@ class Test(unittest.TestCase):
             self.value = value
             self.next  = next
 
-    # def test_linked_list_with_no_cycle(self):
-    #     fourth = Test.LinkedListNode(4)
-    #     third = Test.LinkedListNode(3, fourth)
-    #     second = Test.LinkedListNode(2, third)
-    #     first = Test.LinkedListNode(1, second)
-    #     result = contains_cycle(first)
-    #     self.assertFalse(result)
-    #
-    # def test_cycle_loops_to_beginning(self):
-    #     fourth = Test.LinkedListNode(4)
-    #     third = Test.LinkedListNode(3, fourth)
-    #     second = Test.LinkedListNode(2, third)
-    #     first = Test.LinkedListNode(1, second)
-    #     fourth.next = first
-    #     result = contains_cycle(first)
-    #     self.assertTrue(result)
-    #
-    # def test_cycle_loops_to_middle(self):
-    #     fifth = Test.LinkedListNode(5)
-    #     fourth = Test.LinkedListNode(4, fifth)
-    #     third = Test.LinkedListNode(3, fourth)
-    #     second = Test.LinkedListNode(2, third)
-    #     first = Test.LinkedListNode(1, second)
-    #     fifth.next = third
-    #     result = contains_cycle(first)
-    #     self.assertTrue(result)
+    def test_linked_list_with_no_cycle(self):
+        fourth = Test.LinkedListNode(4)
+        third = Test.LinkedListNode(3, fourth)
+        second = Test.LinkedListNode(2, third)
+        first = Test.LinkedListNode(1, second)
+        result = contains_cycle(first)
+        self.assertFalse(result)
 
-    # def test_two_node_cycle_at_end(self):
-    #     fifth = Test.LinkedListNode(5)
-    #     fourth = Test.LinkedListNode(4, fifth)
-    #     third = Test.LinkedListNode(3, fourth)
-    #     second = Test.LinkedListNode(2, third)
-    #     first = Test.LinkedListNode(1, second)
-    #     fifth.next = fourth
-    #     result = contains_cycle(first)
-    #     self.assertTrue(result)
-    #
-    # def test_empty_list(self):
-    #     result = contains_cycle(None)
-    #     self.assertFalse(result)
+    def test_cycle_loops_to_beginning(self):
+        fourth = Test.LinkedListNode(4)
+        third = Test.LinkedListNode(3, fourth)
+        second = Test.LinkedListNode(2, third)
+        first = Test.LinkedListNode(1, second)
+        fourth.next = first
+        result = contains_cycle(first)
+        self.assertTrue(result)
 
-    # def test_one_element_linked_list_no_cycle(self):
-    #     first = Test.LinkedListNode(1)
-    #     result = contains_cycle(first)
-    #     self.assertFalse(result)
+    def test_cycle_loops_to_middle(self):
+        fifth = Test.LinkedListNode(5)
+        fourth = Test.LinkedListNode(4, fifth)
+        third = Test.LinkedListNode(3, fourth)
+        second = Test.LinkedListNode(2, third)
+        first = Test.LinkedListNode(1, second)
+        fifth.next = third
+        result = contains_cycle(first)
+        self.assertTrue(result)
+
+    def test_two_node_cycle_at_end(self):
+        fifth = Test.LinkedListNode(5)
+        fourth = Test.LinkedListNode(4, fifth)
+        third = Test.LinkedListNode(3, fourth)
+        second = Test.LinkedListNode(2, third)
+        first = Test.LinkedListNode(1, second)
+        fifth.next = fourth
+        result = contains_cycle(first)
+        self.assertTrue(result)
+
+    def test_empty_list(self):
+        result = contains_cycle(None)
+        self.assertFalse(result)
+
+    def test_one_element_linked_list_no_cycle(self):
+        first = Test.LinkedListNode(1)
+        result = contains_cycle(first)
+        self.assertFalse(result)
 
     def test_one_element_linked_list_cycle(self):
         first = Test.LinkedListNode(1)
